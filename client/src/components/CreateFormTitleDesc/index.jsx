c5275fa15198aa36453f6c49a2e23dbe25381123
Cams-Plan <ca.francis@outlook.com>
2023-12-13 03:32:42 +0000

    submit button added and removals

--- client/src/components/CreateFormTitleDesc/index.jsx
+++ client/src/components/CreateFormTitleDesc/index.jsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View,TextInput, FlatList, TouchableOpacity, Button } from 'react-native'
+import { StyleSheet, Text, View,TextInput, FlatList, TouchableOpacity, Button, Pressable } from 'react-native'
 import React from 'react'
 import CreateDrillOption from '../CreateDrillOption';
+import { MaterialIcons, Octicons } from '@expo/vector-icons'
 
 import { FieldArray, Formik } from 'formik'
 import * as yup from "yup"
@@ -31,15 +32,15 @@ export default function CreateFormTitleDesc({ addDrill }) {
     }
   ];
 
-  const handleStep = (currentSteps) => {
-
-  }
+  const newStep = (currentSteps) => ({
+    currentSteps: ''
+  })
 
   return (
     
     <View style={styles.formContainer} >
       <Formik 
-      initialValues={{title: '', description: '', primaryKPI: null, secondaryKPI: null, players: null, type: null, stepByStep: [''],}}
+      initialValues={{title: '', description: '', primaryKPI: null, secondaryKPI: null, players: null, type: null, stepByStep: []}}
       validationSchema={createSchema}
       >
         {(props)=> (
@@ -62,93 +63,88 @@ export default function CreateFormTitleDesc({ addDrill }) {
                     />
                   <Text style={{textAlign: "center", margin: 5}} >Characters remaining {150 - props.values.description.length}</Text>
                 </View>
-
-                <View style={styles.metaContainer}>
-                  {/* ICONS */}
-                  {options.map((opt => {
-                    return (
-                    <View key={opt.key} style={styles.optionsGroupContainer} >
-                      <Text style={styles.optionGroupText}>{opt.optionGroup}</Text>
-                          <View style={styles.optionsGroup} >
-                          {opt.options.map((option)=> (
-                              <TouchableOpacity 
-                              onPress={()=> props.setFieldValue(opt.field, option)}>
-                              <CreateDrillOption 
-                              key={option.key} 
-                              option={option}
-                              selected={props.values[opt.field]}
-                              />
-                              </TouchableOpacity>
-                          ))}
-                          </View>
-                    </View>
-                    )
-                  }))}
-                  {/* <FlatList
-                    data={options}
-                    style={styles.list}
-                    keyExtractor={(item) => item.key}
-                    numColumns={2}
-                    columnWrapperStyle={{
-                      width: '100%',
-                      justifyContent: 'space-evenly'
-                    }}
-                    renderItem={({ item })=> (
-                      <View style={styles.optionsGroupContainer} >
-                          <Text style={styles.optionGroupText}>{item.optionGroup}</Text>
-                          <View style={styles.optionsGroup} >
-                          {item.options.map((option)=> (
-                              <TouchableOpacity 
-                              onPress={()=> props.setFieldValue(item.field, option)}>
-                              <CreateDrillOption 
-                              key={option.key} 
-                              option={option}
-                              selected={props.values[item.field]}
-                              />
-                              </TouchableOpacity>
-                          ))}
-                          </View>
+                <View>
+                  <Text style={styles.sectionTitle} >Step-By-Step</Text>
+                  <View style={styles.metaContainer}>
+                    {/* ICONS */}
+                    {options.map((opt => {
+                      return (
+                      <View key={opt.key} style={styles.optionsGroupContainer} >
+                        <Text style={styles.optionGroupText}>{opt.optionGroup}</Text>
+                            <View style={styles.optionsGroup} >
+                            {opt.options.map((option)=> (
+                                <TouchableOpacity 
+                                onPress={()=> props.setFieldValue(opt.field, option)}>
+                                <CreateDrillOption 
+                                key={option.key} 
+                                option={option}
+                                selected={props.values[opt.field]}
+                                />
+                                </TouchableOpacity>
+                            ))}
+                            </View>
                       </View>
-                    )}
-                    /> */}
+                      )
+                    }))}
+                  </View>
                 </View>
                 <View >
-                  <Text>Step-By-Step</Text>
-                  <TextInput 
+                  <Text style={styles.sectionTitle} >Step-By-Step</Text>
+                  {/* <TextInput 
                     style={styles.stepByStepInput}
                     multiline={true}
                     placeholder='Step 1'
                     onChangeText={props.handleChange('stepByStep')}
                     value={props.values.stepByStep}
                     maxLength={150}
-                    />
+                    /> */}
                 </View>
+                <View style={styles.stepsContainer} >
                 <FieldArray
                 name="stepByStep"
                 render={arrayHelpers => (
                   <>
                     {props.values.stepByStep.map((step, index) => (
-                      <View key={index}>
+                      <View key={index} style={styles.step}>
                         {/** both these conventions do the same */}
                         <TextInput 
                           style={styles.stepByStepInput}
                           multiline={true}
-                          placeholder={`step${index+1}`}
+                          placeholder={`Step ${index+1}`}
                           onChangeText={props.handleChange(`stepByStep[${index}]`)}
                           value={props.values.stepByStep[index]}
                           maxLength={150}
                         />
-                        <Button title='-' onPress={() => arrayHelpers.remove(index)}/>
-                        <Button
-                          title='+'
-                          onPress={() =>
-                          arrayHelpers.push(index, props.values.stepByStep)} />
+                        <TouchableOpacity 
+                        onPress={() => arrayHelpers.remove(index)}  >
+                          <MaterialIcons 
+                          name="remove-circle-outline" 
+                          size={32} color="darkred" 
+                          style={styles.removeIcon} />
+                        </TouchableOpacity>
                       </View>
                     ))}
+                    <TouchableOpacity 
+                    onPress={() => props.setFieldValue('stepByStep', [...props.values.stepByStep, newStep(`step${props.values.stepByStep.length}`)])} >
+                      <Octicons 
+                      name="diff-added"
+                      size={40} color="#9e9b90" 
+                      style={styles.addStepIcon}
+                      />
+                    </TouchableOpacity>
+                    {/* <MaterialIcons 
+                    name="add-circle-outline" 
+                    size={40} color="black" 
+                    style={styles.addStepIcon}
+                    onPress={() => props.setFieldValue('stepByStep', [...props.values.stepByStep, newStep(`step${props.values.stepByStep.length}`)])} /> */}
                   </>
                 )}
 
                 />
+                </View>
+                <TouchableOpacity style={styles.submitButton}>
+                  <Text style={styles.submitText} >Create</Text>
+                </TouchableOpacity>
             </View>
         )}
       </Formik>
@@ -214,15 +210,51 @@ const styles = StyleSheet.create({
     },
     stepByStepInput: {
       alignSelf: "center",
-      width: "100%",
-      height: 90,
+      width: "80%",
+      height: 60,
       padding: 5,
       borderRadius: 10,
       backgroundColor: "#FBCB9320",
-      marginTop: 15,
       textAlign: "center",
       fontSize: 20,
       borderColor: "#C5882C",
       borderWidth: 1
   },
+  sectionTitle: {
+    marginTop: 15,
+    fontSize: 17,
+    textAlign: "center"
+  },
+  stepsContainer: {
+    marginTop: 15,
+  },
+  step: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 15,
+  },
+  removeIcon: {
+    marginLeft: 10
+  },
+  addStepIcon: {
+    textAlign: "center",
+    marginBottom: 15
+  },
+  submitButton: {
+    alignSelf: 'center',
+    padding: 5,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'green',
+    width: 80,
+  },
+  submitText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    textAlign: "center",
+  }
 })
